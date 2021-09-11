0b71cfcd88a5a9e42db2baadfa597c19f30d5683
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-09-11 14:48:26 +0600

    test

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ app.get("/", (req, res) => {
   res.send("Hello from db it's working working");
 });
 
-
 const upload = multer({ storage: storage });
 app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
